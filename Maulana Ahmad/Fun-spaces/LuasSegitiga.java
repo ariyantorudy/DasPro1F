8521b0f650b625d545b3ecd952911cd1db2a0db6
Maulana <maulanahmad2287@gmail.com>
2024-10-25 17:05:20 +0700

    Add files via upload

---
+++ Maulana Ahmad/Fun-spaces/LuasSegitiga.java
@@ -0,0 +1,14 @@
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        // deklarasi main variable
+        double alas, tinggi, luas;
+
+        //inisialisasi variable
+        alas=5;
+        tinggi=10;
+        luas=0.5*alas*tinggi;
+
+        //cetak hasil
+        System.out.println("luas segitiga adalah" +luas);
+    }
+}
