f0c2911a591f49211af34dd071a1d84cb2134969
EarlyyChel <earlyyychelsealia@gmail.com>
2024-10-25 10:13:25 +0700

    Add files via upload

---
+++ Early Chelselia/JobSheet1/Latihan10.java
@@ -0,0 +1,7 @@
+public class Latihan10 {
+    
+    public static void main(String[] args) {
+        System.out.print("Nama saya Early Chelsealia Az Zahra");
+        System.out.print("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
