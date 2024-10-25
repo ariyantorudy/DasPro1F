e1e9552a8f1f783166dcda777b221e7c46a0b1b5
LiliyanPramudita16 <liliyanpramudita1@gmail.com>
2024-10-25 21:17:04 +0700

    Add files via upload

---
+++ Liliyan Pramudita/Jobsheet1/daspro-jobsheet1/Latihan16.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan16
+ */
+public class Latihan16 {
+
+        public static void main(String[] args){
+            System.out.println("Nama Saya Liiyan Pramudita");
+            System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+        }
+}
---
+++ Liliyan Pramudita/Jobsheet1/daspro-jobsheet1/Tugas16.java
@@ -0,0 +1,15 @@
+public class Tugas16 {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi-POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama        : Liliyan Pramudita");
+        System.out.println("NIM         : 244107060096");
+        System.out.println("Kelas       : 1F");
+        System.out.println("No. Absen   : 16 ");
+        System.out.println("Prodi       : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat      : Ponorogo, Jawa Timur ");
+        System.out.println("No. HP      : 085733433620");
+        System.out.println("Hobi        : Membaca");
+    }
+}
