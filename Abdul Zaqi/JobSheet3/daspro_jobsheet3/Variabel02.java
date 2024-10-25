0b5f1715e24ffde6b9fac47d15cd2e26b46522ff
zaqqii <syaqiqurrochman@gmail.com>
2024-10-25 10:14:23 +0700

    Add files via upload

---
+++ Abdul Zaqi/JobSheet3/daspro_jobsheet3/Variabel02.java
@@ -0,0 +1,17 @@
+public class Variabel02 {
+    public static void main(String[] args) {
+        String salahSatuHobySaya = "menggambar";
+        boolean isPandai = true;
+        char jenisKelamin ='L';
+        byte _umurSayaSekarang = 20;
+        double $ipk= 3.24, tinggi = 1.78;
+        System.out.println(salahSatuHobySaya);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin ini: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    
+    }
+
+}
