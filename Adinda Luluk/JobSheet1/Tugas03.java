221f57cc70fff52f18d4f4117c7619197d6bcc11
aadinda75 <dindaluluk209@gmail.com>
2024-10-25 10:57:27 +0700

    Add files via upload

---
+++ Adinda Luluk/JobSheet1/Tugas03.java
@@ -0,0 +1,15 @@
+public class Tugas03 {
+    public static void main (String[] args){
+        System.out.println ("--------------------------------------------------------");
+        System.out.println ("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println ("--------------------------------------------------------");
+        System.out.println ("Nama       : Adinda Luluk Hanifah");
+        System.out.println ("NIM        : 244107060137");
+        System.out.println ("No Absen   : 03");
+        System.out.println ("Prodi      : D-IV Sistem Informasi Bisnis");
+        System.out.println ("Alamat     : Dsn. Bugis, Sabtorenggo, Pakis, Kabupaten Malang");
+        System.out.println ("No HP      : 082311458547");
+        System.out.println ("Hobi       : Olahraga");
+        
+    }
+}
