7913a844dd2ad02031e97ee55ccedbedc1f9a665
Maulana <maulanahmad2287@gmail.com>
2024-10-25 17:25:34 +0700

    Add files via upload

---
+++ Maulana Ahmad/Daspro-jobsheet3/ContohTipeData17.java
@@ -0,0 +1,19 @@
+public class ContohTipeData17 {
+    public static void main(String[] args) {
+        char golonganDarah = 'A';
+        byte jarak = (byte) 130;
+        short jumlahPendudukDalamSatuanDusun = 1025;
+        float suhu = 60.50F;
+        double berat = 0.5467812345;
+        long saldo = 150000000;
+        int angkaDesimal = 0x10;
+
+        System.out.println("Golongan darah\t\t\t\t\t\t: " + (byte) golonganDarah);
+        System.out.println("jarak\t\t\t\t\t\t\t\t\t\t\t: " +  jarak);
+        System.out.println("jumlahPendudukdalamSatuanDusun\t: " + jumlahPendudukDalamSatuanDusun);
+        System.out.println("berat\t\t\t\t\t\t\t\t\t\t\t\t\t: " + (float) berat);
+        System.out.println("saldo\t\t\t\t\t\t\t\t\t\t\t\t\t: " + saldo);
+        System.out.println("angkaDesimal\t\t\t\t\t\t\t\t\t: " + angkaDesimal);
+
+    }
+}
