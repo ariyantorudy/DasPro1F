67b86526263421ea629e2fbb3e439a2fdb8c5d30
N0p3l <naufalranifalutama@gmail.com>
2024-10-25 16:37:47 +0700

    Add files via upload

---
+++ Muhammad Naufal/jobsheet3/BankNo19.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BankNo19 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int jml_tabungan_awal, lama menabung;
+        double prosentase_bunga =0.02, bunga, jml_tabungan_akhir;
+        
+
+    }
+}
---
+++ Muhammad Naufal/jobsheet3/ContohVariabelNoAbsen.java
@@ -0,0 +1,16 @@
+public class ContohVariabelNoAbsen {
+    public static void main(String[] args) {
+        String salahsatuhobbysayaadalah = "Bermain petak umpet";
+boolean ispandai = true;
+char jeniskelamin = 'L';
+byte _umurSayaSekarang = 20;
+double $ipk = 3.24, tinggi = 1.78;
+System.out.println(salahsatuhobbysayaadalah);
+System.out.println("apakah pandai?" + ispandai);
+System.out.println("jenis kelamin: " + jeniskelamin);
+System.out.println("umurku saat ini: " + _umurSayaSekarang);
+System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+}
+
