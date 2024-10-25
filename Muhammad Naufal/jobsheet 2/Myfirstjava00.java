f069222e169a1b97634dc5507793769acd7827ee
N0p3l <naufalranifalutama@gmail.com>
2024-10-25 16:14:30 +0700

    Delete Muhammad Naufal/jobsheet 2/Myfirstjava00.java

--- Muhammad Naufal/jobsheet 2/Myfirstjava00.java
+++
@@ -1,7 +0,0 @@
-public class Myfirstjava00 {
-    public static void main(String[] args) {
-        System.out.println("Nama Saya Muhammad Naufal");
-        System.out.println("saya adalah mahasiswa baru jurusan Teknologi Informasi-POLINEMA");
-    }
-    
-}
