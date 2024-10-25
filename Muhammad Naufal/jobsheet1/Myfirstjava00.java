0088a60717e4cd12afd5bd058409a3da423b25f9
N0p3l <naufalranifalutama@gmail.com>
2024-10-25 16:21:36 +0700

    Add files via upload

---
+++ Muhammad Naufal/jobsheet1/Myfirstjava00.java
@@ -0,0 +1,7 @@
+public class Myfirstjava00 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Muhammad Naufal");
+        System.out.println("saya adalah mahasiswa baru jurusan Teknologi Informasi-POLINEMA");
+    }
+    
+}
