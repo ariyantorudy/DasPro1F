2c780bab3b3fe72781a5610b26563de455bdd5ab
fathinatiqah <110433060+fathinatiqah@users.noreply.github.com>
2024-10-25 11:07:32 +0700

    Add files via upload

---
+++ Atiqah Fathin/JobSheet1/MyFirstJava05.java
@@ -0,0 +1,18 @@
+/**
+ * MyFirstJava05
+ */
+public class MyFirstJava05 {
+    public static void main(String[] args) {
+
+        System.out.println ("--------------------------------------------------------");
+        System.out.println ("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println ("--------------------------------------------------------");
+        System.out.println ("Nama       : Atiqah Fathin Fauziyyah");
+        System.out.println ("NIM        : 244107060031");
+        System.out.println ("No Absen   : 05");
+        System.out.println ("Prodi      : D-IV Sistem Informasi Bisnis");
+        System.out.println ("Alamat     : Jl. Kavaleri Dalam No.12C, Malang");
+        System.out.println ("No HP      : 087754724236");
+        System.out.println ("Hobi       : Menyanyi");
+    } 
+}
