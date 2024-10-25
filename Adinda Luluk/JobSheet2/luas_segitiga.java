4e00903b0a69aa618813bd95fac1552c5e2abeff
aadinda75 <dindaluluk209@gmail.com>
2024-10-25 11:03:22 +0700

    Add files via upload

---
+++ Adinda Luluk/JobSheet2/luas_segitiga.java
@@ -0,0 +1,16 @@
+public class luas_segitiga {
+
+    public static void main(String[] args) {
+        // deklarasi variable
+        double alas, tinggi, luas;
+
+        //instalasi variable
+        alas=5;
+        tinggi=10;
+        luas=0.5*alas*tinggi;
+        
+        //cetak hasil
+        System.out.println("luas segitiga adalah" +luas);
+    }
+}
+
