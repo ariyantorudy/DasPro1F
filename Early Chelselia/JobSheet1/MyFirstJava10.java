2bb9d3ce9291dc817c9a6e0c53dd9181c01d8487
EarlyyChel <earlyyychelsealia@gmail.com>
2024-10-25 10:08:40 +0700

    Add files via upload

---
+++ Early Chelselia/JobSheet1/MyFirstJava10.java
@@ -0,0 +1,15 @@
+public class MyFirstJava10 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Early Chelsealia Az Zahra");
+        System.out.println("NIM      : 244107060128");
+        System.out.println("Kelas    : 1F");
+        System.out.println("No Absen : 10");
+        System.out.println("Prodi    : DIV-Sistem Informasi Bisnis");
+        System.out.println("Alamat   : Jl. Simpang Bondowoso No.12");
+        System.out.println("No HP    : 081336528543");
+        System.out.println("Hobi     : Bermain Billiard dan Badminton");
+    }
+}
