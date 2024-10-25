0fbaa0edbde4240279aa638f71bb75da3c9ff37e
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2024-10-25 20:39:16 +0700

    Add files via upload

---
+++ Rafif Farrel/daspro-jobsheet3(noncasestudy)/ContohVaribelJava23.java
@@ -0,0 +1,14 @@
+public class ContohVaribelJava23 {
+    public static void main(String[] args) {
+        String salahSatuHobySayaAdalah = "olahraga";
+        boolean isPandai = true;
+        char jenisKelamin ='L';
+        int _umurSayaSekarang = 18;
+        double $ipk = 4.00, tinggi = 1.78;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+        }
+}
