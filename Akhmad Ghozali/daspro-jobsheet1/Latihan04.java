127da08640f9c47fc8f0a44ee0ff1654a6fb7f20
akhmadghozali <akhmadghozali4414@gmail.com>
2024-10-25 10:25:40 +0700

    Add files via upload

---
+++ Akhmad Ghozali/daspro-jobsheet1/Latihan04.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan04
+ */
+public class Latihan04 {
+
+    public static void main (String[] args){
+        System.out.println("Nama saya Akhmad Ghozali");
+        //System.out.println("\nSaya adalah Mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Akhmad Ghozali/daspro-jobsheet1/MyFirstJava04.java
@@ -0,0 +1,19 @@
+public class MyFirstJava04 {
+    public static void main(String[] args) {
+   
+        //System.out.println("Nama Saya Akhmad Ghozali");
+        //System.out.println("Saya adalah Mahasiswa Jurusan Teknologi Informasi");
+
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--DATA MAHASISWA JURUSAN TEKNOLOGI INFORMASI POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama    : Akhmad GHozali");
+        System.out.println("NIM     : 244107060112");
+        System.out.println("Kelas   : 1-F");
+        System.out.println("No Absen: 04");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Rt 09 Rw 04 Desa GajahBendo Kec.Beji Kab.Pasuruan");
+        System.out.println("No HP   : 085733394361");
+        System.out.println("Hobi    : Bermain Game");
+}
+}
