023b27349899ae188bf57f6cd1fdd7e10874a0d8
LiliyanPramudita16 <liliyanpramudita1@gmail.com>
2024-10-25 21:30:12 +0700

    Add files via upload

---
+++ Liliyan Pramudita/jobsheet3/daspro-jobsheet3-12.09.24/Segitiga16.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Segitiga16 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alas, tinggi;
+        float luas;
+
+        System.out.print("Masukkan alas: ");
+        alas = sc.nextInt();
+
+        System.out.print("Masukkan tinggi: ");
+        tinggi = sc.nextInt();
+        luas = (float) (alas*tinggi) / 2;
+        System.out.println("Luas segitiga adalah: " + luas);
+        sc.close();
+    }
+}
