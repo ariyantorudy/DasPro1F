28447956cd8df7fc62554e0b4d328d8c93f04d34
Fajar Kurnia Putra <fajarkurniaputrasib@gmail.com>
2024-10-25 09:03:26 +0700

    Commit 1: Jobsheet 1

---
+++ Fajar Kurnia/JobSheet1/MyFirstJava11.java
@@ -0,0 +1,19 @@
+public class MyFirstJava11 {
+    public static void main (String [] args){
+
+        // System.out.println("Nama Saya Fajar Kurnia Putra");
+        // System.out.println("Saya adalah Mahasiswa Jurusan Teknologi Informasi");
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Fajar Kurnia Putra");
+        System.out.println("NIM     : 244107060074");
+        System.out.println("Kelas   : SIB 1F");
+        System.out.println("No Absen: 11");
+        System.out.println("Prodi   : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Jl. Ranugrati no 24B");
+        System.out.println("No HP   : 087857065253");
+        System.out.println("Hobi    : Nulis Novel, Baca Komik, dan Nonton Film");
+    }
+}
