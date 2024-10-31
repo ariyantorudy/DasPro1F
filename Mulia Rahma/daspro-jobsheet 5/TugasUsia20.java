27e8feb1425a56acf2f87aa75e5e4051ba1866a5
muliawell <raragombel01@gmail.com>
2024-10-31 13:10:40 +0700

    Add files via upload

---
+++ Mulia Rahma/daspro-jobsheet 5/TugasUsia20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TugasUsia20 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Masukkan Usia Anda: ");
+      int usia  =  sc.nextInt();
+
+        if (usia >= 0 && usia <= 12) {
+            System.out.println(" Anak-anak");
+            
+        } else if (usia >- 13 && usia <= 19) {
+            System.out.println(" Remaja");
+            
+        } else if (usia >- 20 && usia <= 64) {
+            System.out.println(" Dewasa");
+            
+        } else if (usia >- 65 && usia <= 1000){
+            System.out.println(" Lansia");
+            
+        } else {
+            System.out.println(" Usia tidak valid,Silahkan coba lagi");
+        }   
+     
+    }
+}
