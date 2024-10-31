4d2061afb2bc25ec9d54f7ac7d025ba000abdb70
aadinda75 <dindaluluk209@gmail.com>
2024-10-31 21:35:45 +0700

    Add files via upload

---
+++ Adinda Luluk/JobSheet5/PemilihanHariDenganIf03.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        String dayName;
+
+        System.out.print("Masukkan bilangan (1-7): ");
+        number = sc.nextInt();
+
+        if (number >= 1 && number <= 5) {
+            dayName = "Weekday";
+        } else if (number == 6 || number == 7) {
+            dayName = "Weekend";
+        } else {
+            dayName = "Invalid Number";
+        }
+        
+        System.out.println(dayName);
+
+        sc.close();
+    }
+}
---
+++ Adinda Luluk/JobSheet5/PemilihanOperator03.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PemilihanOperator03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.print("Masukkan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukkan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        operator = sc.next().charAt(0);
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;
+        } else {
+            System.out.println("Operator tidak valid!");
+        }
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+    
+        sc.close();
+    }
+}
